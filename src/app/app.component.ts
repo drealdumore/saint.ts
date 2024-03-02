cbd4f44e7667edc4a4f2bed0a181f23cc7528113
drealdumore <sireroyce1@gmail.com>
2024-03-02 23:45:38 +0100

    Floating mobile nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+
 import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainNavComponent,FooterComponent],
+  imports: [RouterOutlet, MainNavComponent,FooterComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
