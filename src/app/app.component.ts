ddc4a9897cfa6b44e564cffc19cc272faf65dcaf
drealdumore <sireroyce1@gmail.com>
2024-03-04 14:41:51 +0100

    added analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,17 @@ import { RouterModule } from '@angular/router';
 
 import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { inject } from '@vercel/analytics';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   standalone: true,
-  imports: [RouterOutlet, MainNavComponent,FooterComponent, RouterModule],
+  imports: [RouterOutlet, MainNavComponent, FooterComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Saint | Angular Dev';
+  constructor() {
+    inject();
+  }
 }
